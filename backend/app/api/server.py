414b9cebbdfa2c5540f4ce2083a6c55a373144a6
baxromov1 <baxromov.shahzodbek@gmail.com>
2023-02-15 22:55:29 +0500

    add changes

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.api.routes import router as api_router
+from app.core import config, tasks
 
 
 def get_application():
-    app = FastAPI(title="Lesson2")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
     app.add_middleware(
         CORSMiddleware,
@@ -13,7 +14,10 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(api_router, prefix="/api")
+
     return app
 
 
